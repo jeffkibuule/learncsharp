2dd60186887481a333d1bcba3d36b982ab893fb2
Joefrey Kibuule <jeff.kibuule@outlook.com>
2014-08-24 15:52:45 -0500

    Remove ImmutableCounter.cs and update Coutner.cs

--- ch3/ch3/Counter.cs
+++ ch3/ch3/Counter.cs
@@ -1,13 +1,35 @@
 ﻿public class Counter
 {
-    private int _count;
+    private readonly int _count;
     private static int _totalCount;
 
-    public int GetNextValue()
+    public Counter()
     {
-        _count += 1;
+        _count = 0;
+    }
+
+    private Counter(int count)
+    {
+        _count = count;
+    }
+
+    public Counter GetNextValue()
+    {
         _totalCount += 1;
-        return _count;
+        return new Counter(_count + 1);
+    }
+
+    public static Counter operator ++(Counter input)
+    {
+        return input.GetNextValue();
+    }
+
+    public int Count
+    {
+        get
+        {
+            return _count;
+        }
     }
 
     public static int TotalCount
--- ch3/ch3/Program.cs
+++ ch3/ch3/Program.cs
@@ -11,14 +11,20 @@ namespace ch3
         static void Main(string[] args)
         {
             Counter c1 = new Counter();
+            c1++;
             Counter c2 = c1;
-            Console.WriteLine("c1: " + c1.GetNextValue());
-            Console.WriteLine("c1: " + c1.GetNextValue());
+            Counter c3 = new Counter();
+            c3++;
 
-            Console.WriteLine("c2: " + c2.GetNextValue());
-            Console.WriteLine(Counter.TotalCount);
-            
-            Console.WriteLine("c1: " + c1.GetNextValue());
+            Console.WriteLine(c1.Count);
+            Console.WriteLine(c2.Count);
+            Console.WriteLine(c3.Count);
+            Console.WriteLine(c1 == c2);
+            Console.WriteLine(c1 == c3);
+            Console.WriteLine(c2 == c3);
+            Console.WriteLine(object.ReferenceEquals(c1, c2));
+            Console.WriteLine(object.ReferenceEquals(c1, c3));
+            Console.WriteLine(object.ReferenceEquals(c2, c3));
         }
     }
 }
