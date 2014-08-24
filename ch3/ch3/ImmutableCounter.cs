26040dfbfc87cc31c3fd936cf1d24ef6862a0f9b
Joefrey Kibuule <jeff.kibuule@outlook.com>
2014-08-24 15:45:24 -0500

    Add ch3: immutable counter

---
+++ ch3/ch3/ImmutableCounter.cs
@@ -0,0 +1,36 @@
+﻿public class ImmutableCounter
+{
+    private readonly int _count;
+    private static int _totalCount;
+
+    public ImmutableCounter()
+    {
+        _count = 0;
+    }
+
+    private ImmutableCounter(int count)
+    {
+        _count = 0;
+    }
+
+    public ImmutableCounter GetNextValue()
+    {
+        _totalCount += 1;
+        return new ImmutableCounter(_count + 1);
+    }
+
+    public static ImmutableCounter operator ++(ImmutableCounter input)
+    {
+        return input.GetNextValue();
+    }
+
+    public int Count
+    {
+        get
+        {
+            return _count;
+        }
+    }
+
+
+}
