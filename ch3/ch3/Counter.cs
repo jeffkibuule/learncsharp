11f6ef0b9fe425161144f148a40bc015812a13f0
Joefrey Kibuule <jeff.kibuule@outlook.com>
2014-08-24 15:45:09 -0500

    Add ch3: counter

---
+++ ch3/ch3/Counter.cs
@@ -0,0 +1,20 @@
+﻿public class Counter
+{
+    private int _count;
+    private static int _totalCount;
+
+    public int GetNextValue()
+    {
+        _count += 1;
+        _totalCount += 1;
+        return _count;
+    }
+
+    public static int TotalCount
+    {
+        get
+        {
+            return _totalCount;
+        }
+    }
+}
---
+++ ch3/ch3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter c1 = new Counter();
+            Counter c2 = c1;
+            Console.WriteLine("c1: " + c1.GetNextValue());
+            Console.WriteLine("c1: " + c1.GetNextValue());
+
+            Console.WriteLine("c2: " + c2.GetNextValue());
+            Console.WriteLine(Counter.TotalCount);
+            
+            Console.WriteLine("c1: " + c1.GetNextValue());
+        }
+    }
+}
